b42825e9d2810d93ff1ec707de3e95aa81384950
azri fathul <azrifathulabar@gmail.com>
2021-06-28 17:24:00 +0700

    yo

--- app.js
+++ app.js
@@ -11,40 +11,40 @@ const io = require("socket.io")(server);
 
 io.sockets.on("error", (e) => console.log(e));
 io.sockets.on("connection", (socket) => {
-  socket.on("broadcaster-camera1", () => {
+  socket.on("broadcaster-toddler", () => {
     broadcaster = socket.id;
-    socket.broadcast.emit("broadcaster-camera1");
+    socket.broadcast.emit("broadcaster-toddler");
   });
-  socket.on("watcher-camera1", () => {
-    socket.to(broadcaster).emit("watcher-camera1", socket.id);
+  socket.on("watcher-toddler", () => {
+    socket.to(broadcaster).emit("watcher-toddler", socket.id);
   });
-  socket.on("offer-camera1", (id, message) => {
-    socket.to(id).emit("offer-camera1", socket.id, message);
+  socket.on("offer-toddler", (id, message) => {
+    socket.to(id).emit("offer-toddler", socket.id, message);
   });
-  socket.on("answer-camera1", (id, message) => {
-    socket.to(id).emit("answer-camera1", socket.id, message);
+  socket.on("answer-toddler", (id, message) => {
+    socket.to(id).emit("answer-toddler", socket.id, message);
   });
-  socket.on("candidate-camera1", (id, message) => {
-    socket.to(id).emit("candidate-camera1", socket.id, message);
+  socket.on("candidate-toddler", (id, message) => {
+    socket.to(id).emit("candidate-toddler", socket.id, message);
   });
 
   //camera2
 
-  socket.on("broadcaster-camera2", () => {
+  socket.on("broadcaster-infant", () => {
     broadcaster = socket.id;
-    socket.broadcast.emit("broadcaster-camera2");
+    socket.broadcast.emit("broadcaster-infant");
   });
-  socket.on("watcher-camera2", () => {
-    socket.to(broadcaster).emit("watcher-camera2", socket.id);
+  socket.on("watcher-infant", () => {
+    socket.to(broadcaster).emit("watcher-infant", socket.id);
   });
-  socket.on("offer-camera2", (id, message) => {
-    socket.to(id).emit("offer-camera2", socket.id, message);
+  socket.on("offer-infant", (id, message) => {
+    socket.to(id).emit("offer-infant", socket.id, message);
   });
-  socket.on("answer-camera2", (id, message) => {
-    socket.to(id).emit("answer-camera2", socket.id, message);
+  socket.on("answer-infant", (id, message) => {
+    socket.to(id).emit("answer-infant", socket.id, message);
   });
-  socket.on("candidate-camera2", (id, message) => {
-    socket.to(id).emit("candidate-camera2", socket.id, message);
+  socket.on("candidate-infant", (id, message) => {
+    socket.to(id).emit("candidate-infant", socket.id, message);
   });
   socket.on("disconnect", () => {
     socket.to(broadcaster).emit("disconnectPeer", socket.id);
